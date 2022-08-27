b95cccf316043d67c812db3e86ea138c195756db
Jose Daniel Suarez Medina <1jose1s1m@gmail.com>
2022-08-27 11:58:21 -0500

    proyecto funcional

--- Proyecto_Mintic/src/main/java/co/mintic/Main.java
+++ Proyecto_Mintic/src/main/java/co/mintic/Main.java
@@ -3,8 +3,8 @@ package co.mintic;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
 
-    Empresa empresa1= new Empresa("Somatec","diagonal 75DD # 4-75","3004969378","900967105-1");
+        Empresa empresa1 = new Empresa("Somatec", "diagonal 75DD # 4-75", "3004969378", "900967105-1");
         empresa1.obtener_datos();
+    }
 }
