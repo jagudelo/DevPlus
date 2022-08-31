f592868f136cdcabc53b44e02306b5350ef2db24
Jose Agudelo <jose.agudelo@programmersvip.com>
2022-08-30 23:15:26 -0500

    Conexión de base de datos Postgresql Supabase y configuraciones Spring Boot

--- Proyecto_Mintic_Ciclo_3/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
+++ Proyecto_Mintic_Ciclo_3/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
@@ -1,10 +1,21 @@
 package co.Mintic.Ciclo3;
 
+import net.bytebuddy.build.ToStringPlugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+
+	public String hello(){
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
