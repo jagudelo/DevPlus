9c21ebff34cef63acdc34e9e6c13709bb3795ff4
Jose Agudelo <jose.agudelo@programmersvip.com>
2022-08-31 01:04:37 -0500

    Creación clase empresa en proyecto recuperado

--- Proyecto_Mintic_Ciclo_3/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
+++ Proyecto_Mintic_Ciclo_3/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
@@ -1,5 +1,6 @@
 package co.Mintic.Ciclo3;
 
+import co.Mintic.Ciclo3.modelos.Empresa;
 import net.bytebuddy.build.ToStringPlugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class Ciclo3Application {
 
 	@GetMapping("/hello")
-
 	public String hello(){
+
 		return "Hello world";
+	}
+
+	@GetMapping("/test")
+	public String test(){
+		Empresa company = new Empresa("Morfo-Clothing", "Carrera 29 # 78 01", "3507023133", "1013605223");
+
+		return company.getNombre();
 	}
 
 	public static void main(String[] args) {
