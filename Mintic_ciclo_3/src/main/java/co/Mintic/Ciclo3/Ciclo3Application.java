59e6a51752f15cd5106baf9dc293133117007889
Jose Agudelo <jose.agudelo@programmersvip.com>
2022-09-01 23:50:45 -0500

    Creación clase movimiento dinero

--- Mintic_ciclo_3/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
+++ Mintic_ciclo_3/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
@@ -3,6 +3,8 @@ package co.Mintic.Ciclo3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* Comentario */
+
 @SpringBootApplication
 public class Ciclo3Application {
 
