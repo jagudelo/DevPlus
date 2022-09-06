1fc1bd3a6373e2bbb1e853eeae9c02433b1df520
Jose Daniel Suarez Medina <1jose1s1m@gmail.com>
2022-09-05 20:58:09 -0500

    Conexion a base datos postgres

--- mintic_ciclo_3_proyecto/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
+++ mintic_ciclo_3_proyecto/src/main/java/co/Mintic/Ciclo3/Ciclo3Application.java
@@ -1,6 +1,7 @@
 package co.Mintic.Ciclo3;
 
 import jdk.swing.interop.SwingInterOpUtils;
+import net.minidev.json.JSONUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,8 @@ public class Ciclo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+		System.out.println("Hello world");
+
 	}
 
 }
